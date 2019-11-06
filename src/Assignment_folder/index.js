8d72af58dbcb5c2b7e829096d0aad3dbb33ee1bf
Vanamala <vanamala@churchtalk.io>
2019-11-06 17:48:42 +0530

    changes

--- src/Assignment_folder/index.js
+++ src/Assignment_folder/index.js
@@ -11,7 +11,7 @@ class index extends React.Component{
                 articleData : [],
                 search : "",
                 loader : false,
-                totalCount : 62,
+                totalCount : 80,
                 current : 0
             } 
     }
@@ -32,16 +32,19 @@ class index extends React.Component{
       var pages = Math.round(maxcount/10)
       var currentCount = this.state.current;
       currentCount = currentCount + 1 ;
+      var searchedData = this.state.search ? this.state.search : "reactjs"
       if(currentCount > pages)
       console.log("...")
       else{
+      console.log(",,,")
       fetch('https://newsapi.org/v2/everything?q=reactjs&apiKey=363d26dd3d664d199ca63adc371e22aa&pageSize=10&page='+currentCount)
         .then(res => res.json())
         .then((data) => {
           this.setState({ articleData : data.articles,loader:true })
+          console.log("fghj",searchedData,data.articles)
           // call getData() again in 30 seconds
-          this.intervalID = setTimeout(this.getData.bind(this), 30000);
-          console.log(this.intervalID = setTimeout(this.getData.bind(this), 30000))
+          // this.intervalID = setTimeout(this.getData.bind(this), 30000);
+          // console.log(this.intervalID = setTimeout(this.getData.bind(this), 30000))
         })
         .catch(console.log("-"))  
       }
@@ -58,12 +61,13 @@ class index extends React.Component{
     }
 
     render(){
-        var filteredData=this.state.articleData.filter(
-          (search) => {
-            var filter = search
-            return search.source.name.toLowerCase().indexOf(this.state.search) !== -1 && filter;
-          }
-        )
+        // var filteredData=this.state.articleData.filter(
+        //   (search) => {
+        //     var filter = search
+        //     return search.source.name.toLowerCase().indexOf(this.state.search) !== -1 && filter;
+        //   } 
+        // )
+        console.log("log",this.state.articleData)
         return(
             <React.Fragment>
               <InfiniteScroll
@@ -71,7 +75,7 @@ class index extends React.Component{
                   <div className="mainContainer">
                     <div className="subContainer">
                         <div className="" style={{fontSize:"25px",padding:"25px 0px", textAlign:"center"}}>  
-                           Latest News Search  
+                           Latest News Search 
                         </div>
                         <div className="card-container">
                            <div className="searchBar-container">
@@ -82,8 +86,8 @@ class index extends React.Component{
                            </div>
                             <div className="map-container">
                             {
-                                filteredData.length !== 0 ? (
-                                  filteredData.map((value,key)=>{
+                                // this.state.articleData.length !== 0 ? (
+                                  this.state.articleData && this.state.articleData.map((value,key)=>{
                                   return(
                                       <div className="card-contents" key={key}>
                                         <div className="img-container">
@@ -100,11 +104,11 @@ class index extends React.Component{
                                       </div>
                                   )
                                 }) 
-                                ) : (
-                                  <div style={{textAlign:"center",padding:"30px"}}>
-                                    No result found.
-                                  </div>
-                                )
+                                // ) : (
+                                //   <div style={{textAlign:"center",padding:"30px"}}>
+                                //     No result found.
+                                //   </div>
+                                // )
                             }
                             </div>
                         </div>
